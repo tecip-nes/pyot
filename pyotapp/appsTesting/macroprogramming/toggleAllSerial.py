82837edbe0de7fd3c20b41608b00723c86315a70
Andrea Azzara <andreaazzara@yahoo.com>
2013-06-12 11:52:49 +0200

    updated license

--- pyotapp/appsTesting/macroprogramming/toggleAllSerial.py
+++ pyotapp/appsTesting/macroprogramming/toggleAllSerial.py
@@ -1,3 +1,26 @@
+'''
+Copyright (C) 2012,2013 Scuola Superiore Sant'Anna (http://www.sssup.it) 
+and Consorzio Nazionale Interuniversitario per le Telecomunicazioni 
+(http://www.cnit.it).
+
+This file is part of PyoT, an IoT Django-based Macroprogramming Environment.
+
+PyoT is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+  
+PyoT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with PyoT.  If not, see <http://www.gnu.org/licenses/>.
+
+@author: Andrea Azzara' <a.azzara@sssup.it>
+'''
+
 import common
 from pyot.models import Resource
 from datetime import datetime, timedelta
--- pyotapp/pyot/regbackend.py
+++ pyotapp/pyot/regbackend.py
@@ -1,4 +1,23 @@
 '''
+Copyright (C) 2012,2013 Scuola Superiore Sant'Anna (http://www.sssup.it) 
+and Consorzio Nazionale Interuniversitario per le Telecomunicazioni 
+(http://www.cnit.it).
+
+This file is part of PyoT, an IoT Django-based Macroprogramming Environment.
+
+PyoT is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+  
+PyoT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with PyoT.  If not, see <http://www.gnu.org/licenses/>.
+
 @author: Andrea Azzara' <a.azzara@sssup.it>
 '''
 import Forms
