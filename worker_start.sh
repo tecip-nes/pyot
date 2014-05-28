0f49e1948be90be00e2e6bcf2d7f56d985875868
Andrea Azzarà <andreaazzara@yahoo.com>
2014-05-28 20:04:34 +0200

    changed celery hostname

--- worker_start.sh
+++ worker_start.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-venv/bin/python pyotapp/manage.py celeryd -s celery -E -B -l INFO -c 30 -n celery@cooja -Q celery@cooja,celery,periodic --without-heartbeat --without-gossip
+venv/bin/python pyotapp/manage.py celeryd -s celery -E -B -l INFO -c 30 -n cooja@pyot -Q cooja@pyot,celery,periodic --without-heartbeat --without-gossip --Ofair
