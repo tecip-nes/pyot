3b4c0693bfc6ed5a96070ab74f1729337368bc18
Andrea Azzarà <andreaazzara@yahoo.com>
2014-07-09 14:14:09 +0200

    ignore path in landscape

--- pyot/urls.py
+++ pyot/urls.py
@@ -65,6 +65,9 @@ urlpatterns += patterns(
 )
 urlpatterns += patterns(
     '',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(
+        r'^media/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
+    ),
 )
