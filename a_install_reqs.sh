506ce4ae6b6237e040acd923751219b91c58780e
Andrea Azzarà <andreaazzara@yahoo.com>
2015-07-08 17:14:10 +0200

    installation script, add subtree setup

--- a_install_reqs.sh
+++ a_install_reqs.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+git submodule update --init
+cd contiki-tres/tools/cooja
+ant jar
+cd ../
+make tunslip6
+cd ../../
+
+
 sudo pip install virtualenv
 virtualenv venv
 venv/bin/pip install -r requirements.txt
